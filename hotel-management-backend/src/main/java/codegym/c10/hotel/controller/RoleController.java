f99d77e7ccf9fdd7ef8d5dde56aa02ce20bdfd4b
phuhoang.06 <hoangvanphu2006@gmail.com>
2025-04-16 15:05:05 +0700

    checkout

--- hotel-management-backend/src/main/java/codegym/c10/hotel/controller/RoleController.java
+++ hotel-management-backend/src/main/java/codegym/c10/hotel/controller/RoleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/roles")
 public class RoleController {
     @Autowired
